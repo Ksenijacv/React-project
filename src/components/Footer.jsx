73f386752b898d80b7b6415001fe02c002a7db37
Ksenija Cvjetan <ksenija.cvjetan@gmail.com>
2022-12-26 22:41:50 +0100

    Kreirane komponente Pocetna i Footer i uradjen CSS

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+    return (
+      <div>
+
+        <footer className="footer-container"> 
+            <div className="footer-text" >
+                Adresa: Cetinjska 22
+            </div> 
+            <div className="footer-text" >
+                Email: neobicni.zalogaj@gmail.com
+            </div> 
+            <div className="footer-text" >
+                Kontakt telefon: 069 443 2063
+            </div> 
+        </footer> 
+      </div>
+    );
+  }
+
+  export default Footer;
