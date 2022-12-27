31c19e8d2de23be9fcefa33219b0a36ca40f58fb
Ksenija Cvjetan <ksenija.cvjetan@gmail.com>
2022-12-27 14:26:52 +0100

    CSS za dugme Dodaj u Omiljene i ubacena ikonica

--- src/components/Restoran.jsx
+++ src/components/Restoran.jsx
@@ -2,18 +2,17 @@ function Restoran({res}) {
 
 
     return (
-     <div className = 'restorani' >
-
+     
         <div className="card"   >
         <img src={res.slika} className="card-img-top" alt="..."/>
         <div className="card-body">
           <p className="card-title">{res.naziv}</p>
           <p className="card-text">{res.opis} <br /> Ocena: {res.ocena} <br/> Telefon: {res.telefon} <br /> Adresa: {res.adresa} <br /> </p>
-          <a href="#" className="btn btn-primary">Dodaj</a>
+          <h4><a href="#" className="btn btn-primary" style={{marginLeft: "20px" , opacity: "80%", 
+          padding:"10px", paddingLeft: "20px", paddingRight: "20px", border: "3px solid #EEEFEB"}}>Dodaj u omiljene</a></h4>
         </div>
       </div>
 
-    </div>
     );
   }
 
--- src/components/Restorani.jsx
+++ src/components/Restorani.jsx
@@ -2,8 +2,10 @@
 
   function Restorani({restorani}) {
     return (
+      <div className="restorani">
     <div className="row">
           {restorani.map((res)=> <div className="col-sm-3"><Restoran  key={res.id} res={res}></Restoran><br /></div>)}
+    </div>
     </div>
         );
 
